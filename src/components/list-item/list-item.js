d6d2174ed63fa743ba1a609f96f7b13241c0cd4f
Ороз Жумабеков <orozzumabekov@MacBook-Air-Oroz.local>
2023-01-29 19:21:10 +0600

    end

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -1,30 +1,40 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { MyContext } from '../../context/MyContext/MyContext';
 import List from '../list/list';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 const ListItem = (props) => {
-  const { question, id, variants, completed } = props;
-  const { correctCheck, changeCompleted } = useContext(MyContext)
+  const { question, variants,  id} = props;
+  const { correctCheck } = useContext(MyContext)
 
   const handlerClick = (correct) => {
     correctCheck(correct)
   }
 
   return (
-    <li>
+    <li> 
       <h3>{question}</h3>
-      <span>Выберите ответ</span>
-      <List
-        items={variants}
-        renderItem={(elem, index) => {
-          return <div key={index} className='form-control'>
-            <input
-              onClick={() => handlerClick(elem.correct)}
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Выберите ответ</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <List
+            items={variants}
+            renderItem={(elem, index) => <FormControlLabel
+              key={index}
               name={id}
-              type="radio" />
-              <label htmlFor="">{elem.variant}</label>
-          </div>
-        }}
-      />
+              value={elem.value}
+              onClick={() => handlerClick(elem.correct)}
+              control={<Radio />}
+              label={elem.variant} />} />
+        </RadioGroup>
+      </FormControl>
     </li>
   );
 };
